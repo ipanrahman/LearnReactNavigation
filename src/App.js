fb4efe1eb243068559f362a25727af12db958f02
Ipan Taupik Rahman <ipantaupik97@gmail.com>
2019-08-29 18:47:38 +0700

    create bottom navigator on stack navigator

--- src/App.js
+++ src/App.js
@@ -1,114 +1,65 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow
- */
-
-import React, {Fragment} from 'react';
+import React, {Component} from 'react';
 import {
-  SafeAreaView,
-  StyleSheet,
-  ScrollView,
-  View,
-  Text,
-  StatusBar,
-} from 'react-native';
+  createStackNavigator,
+  createAppContainer,
+  createBottomTabNavigator,
+} from 'react-navigation';
 
-import {
-  Header,
-  LearnMoreLinks,
-  Colors,
-  DebugInstructions,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
+import {Icon} from 'react-native-elements';
 
-const App = () => {
-  return (
-    <Fragment>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView>
-        <ScrollView
-          contentInsetAdjustmentBehavior="automatic"
-          style={styles.scrollView}>
-          <Header />
-          {global.HermesInternal == null ? null : (
-            <View style={styles.engine}>
-              <Text style={styles.footer}>Engine: Hermes</Text>
-            </View>
-          )}
-          <View style={styles.body}>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Step One</Text>
-              <Text style={styles.sectionDescription}>
-                Edit <Text style={styles.highlight}>App.js</Text> to change this
-                screen and then come back to see your edits.
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>See Your Changes</Text>
-              <Text style={styles.sectionDescription}>
-                <ReloadInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Debug</Text>
-              <Text style={styles.sectionDescription}>
-                <DebugInstructions />
-              </Text>
-            </View>
-            <View style={styles.sectionContainer}>
-              <Text style={styles.sectionTitle}>Learn More</Text>
-              <Text style={styles.sectionDescription}>
-                Read the docs to discover what to do next:
-              </Text>
-            </View>
-            <LearnMoreLinks />
-          </View>
-        </ScrollView>
-      </SafeAreaView>
-    </Fragment>
-  );
-};
+import HomeScreen from './screens/HomeScreen';
+import SearchScreen from './screens/SearchScreen';
 
-const styles = StyleSheet.create({
-  scrollView: {
-    backgroundColor: Colors.lighter,
+export default class App extends Component {
+  render() {
+    return <AppContainer />;
+  }
+}
+
+const AppBottomNavigator = createBottomTabNavigator({
+  Home: {
+    screen: HomeScreen,
+    navigationOptions: {
+      tabBarIcon: <Icon name="home-outline" type="material-community" />,
+      tabBarOptions: {
+        showLabel: false,
+      },
+    },
   },
-  engine: {
-    position: 'absolute',
-    right: 0,
-  },
-  body: {
-    backgroundColor: Colors.white,
-  },
-  sectionContainer: {
-    marginTop: 32,
-    paddingHorizontal: 24,
-  },
-  sectionTitle: {
-    fontSize: 24,
-    fontWeight: '600',
-    color: Colors.black,
-  },
-  sectionDescription: {
-    marginTop: 8,
-    fontSize: 18,
-    fontWeight: '400',
-    color: Colors.dark,
-  },
-  highlight: {
-    fontWeight: '700',
-  },
-  footer: {
-    color: Colors.dark,
-    fontSize: 12,
-    fontWeight: '600',
-    padding: 4,
-    paddingRight: 12,
-    textAlign: 'right',
+  Search: {
+    screen: SearchScreen,
+    navigationOptions: {
+      tabBarIcon: <Icon name="search" type="feather" />,
+      tabBarOptions: {
+        showLabel: false,
+      },
+    },
   },
 });
 
-export default App;
+const AppStackNavigator = createStackNavigator(
+  {
+    AppBottomNavigator: {screen: AppBottomNavigator},
+  },
+  {
+    defaultNavigationOptions: {
+      headerLeft: (
+        <Icon
+          name="camera-outline"
+          type="material-community"
+          containerStyle={{paddingLeft: 10}}
+        />
+      ),
+      title: 'Instagram',
+      headerRight: (
+        <Icon
+          name="send-o"
+          type="font-awesome"
+          containerStyle={{paddingRight: 10}}
+        />
+      ),
+    },
+  },
+);
+
+const AppContainer = createAppContainer(AppStackNavigator);
